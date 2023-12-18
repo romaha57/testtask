4e70b3fd5b50bc5b83211dd9515953cdfd887ac9
macbook <romes57@gmail.com>
2023-12-18 10:52:39 +0300

    improve the algorithm

--- app/format_answers.py
+++ app/format_answers.py
@@ -12,20 +12,17 @@ def format_items_data(items_data: list[Type[Record]]) -> dict:
     result_data = {}
 
     # группируем словарь по ключу и затем проходимся по сгруппированным элементам для добавления во вложенный словарь
-    for main_category, group in groupby(record_list, lambda elem: elem['main_category']):
-        result_data[main_category] = {}
-        for category, group2 in groupby(group, lambda elem: elem['category']):
-            result_data[main_category][category] = {}
-            for type, group3 in groupby(group2, lambda elem: elem['type']):
-                if type:
-                    result_data[main_category][category][type] = []
-                else:
-                    result_data[main_category][category] = None
-                for i in group3:
-                    if i['ceo']:
-                        result_data[main_category][category][type].append(i['ceo'])
-                    elif result_data[main_category][category] is not None:
-                        result_data[main_category][category][type] = None
+    for elem in record_list:
+        if elem['type'] and elem['ceo']:
+            result_data.setdefault(elem['main_category'], {}).setdefault(elem['category'], {}).setdefault(elem['type'], []).append(elem['ceo'])
+        elif not elem['type']:
+            result_data.setdefault(elem['main_category'], {}).setdefault(elem['category'])
+
+        elif not elem['ceo']:
+            result_data.setdefault(elem['main_category'], {}).setdefault(elem['category'], {}).setdefault(elem['type'])
+        else:
+            result_data.setdefault(elem['main_category'], {}).setdefault(elem['category'], {}).setdefault(elem['type'], {}).setdefault(elem['ceo'])
+
 
     pprint.pprint(result_data)
 
